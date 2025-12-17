c65b210f9952fb4edcf515a3d09c981a9fd5cf73
Alexander Ionov <magneticsun@gmail.com>
2025-12-17 13:43:24 +0300

    v.0.43 taskp3875 step1 новая кампания 56373 специально

--- public/step1.php
+++ public/step1.php
@@ -9,7 +9,10 @@ use App\Controller\Main;
 //$campaign_id = 55750;
 //$campaign_id = 55751;
 //$campaign_id = 55827;
-$campaign_id = 55964;
+//$campaign_id = 55964;
+
+$campaign_id = 56373; //@todo taskp3875 16.12.2025 17:50
+
 
 $file_products = 'D:\site_next\ozon_cmps\data/products_.json';
 $json_file_products = file_get_contents($file_products);
